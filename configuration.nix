8ff3fedd4cb65ea387a49fef389355cb0e03d41f
dooshii <edoosh.yt@gmail.com>
2024-02-29 13:13:31 +1300

    ni

--- configuration.nix
+++ configuration.nix
@@ -32,7 +32,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "dooshii"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
