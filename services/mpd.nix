e6d98298c6da1c6e3c283086e84682ed73062d5e
dooshii <edoosh.yt@gmail.com>
2024-03-01 20:55:50 +1300

    mpd 2

---
+++ services/mpd.nix
@@ -0,0 +1,36 @@
+{pkgs, ...}: let
+  mpdPkg = pkgs.mpd;
+
+  mpdmprisPkg = pkgs.mpd-mpris;
+in {
+  systemd.user.services.mpd = {
+    enable = true;
+    description = "Music Player Daemon";
+
+    after = ["network.target" "sound.target"];
+    wantedBy = ["default.target"];
+
+    environment = "PATH=${config.home.profileDirectory}/bin";
+
+    script = "${mpdPkg}/bin/mpd --no-daemon";
+    serviceConfig = {
+      Type = "notify";
+    };
+  };
+
+  systemd.user.services.mpd-mpris = {
+    enable = true;
+    description = "mpd-mpris: An implementation of the MPRIS protocol for MPD";
+
+    after = ["mpd.service"];
+    requires = ["mpd.service"];
+    wantedBy = ["default.target"];
+
+    script = "${mpdmprisPkg}/bin/mpd-mpris -no-instance";
+    serviceConfig = {
+      Type = "simple";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
+}
