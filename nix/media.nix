8a65d33abeb7ebeb6fdff50b3d5c8ad903e799ae
dooshii <edoosh.yt@gmail.com>
2024-03-02 18:22:07 +1300

    90 current  2024-03-02 18:22:00  24.05.20240301.72f01b2   6.6.18                          *

--- nix/media.nix
+++ nix/media.nix
@@ -7,6 +7,10 @@
   ];
 
   services = {
+    jellyfin = {
+      enable = true;
+      group = "multimedia";
+    };
     prowlarr = {
       enable = true;
       openFirewall = true;
@@ -25,13 +29,16 @@
       enable = true;
       group = "multimedia";
       openFirewall = true;
-      web.enable = true;
-      dataDir = "/data/media/torrent";
-      declarative = true;
-      config = {
-        enabled_plugins = ["Label"];
-        outgoing_interface = "wg1";
+      web = {
+        enable = true;
+        openFirewall = true;
       };
+      # dataDir = "/data/media/torrent";
+      declarative = true;
+      # config = {
+      #   enabled_plugins = ["Label"];
+      #   outgoing_interface = "wg1";
+      # };
       authFile = pkgs.writeText "deluge-auth" ''
         localclient:a7bef72a890:10
         andrew:password:10
