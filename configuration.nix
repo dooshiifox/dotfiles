7513767073c9f1335eadb5be670b77c17ae83fb1
dooshii <edoosh.yt@gmail.com>
2024-03-02 14:30:05 +1300

    86 current  2024-03-02 14:29:58  24.05.20240301.72f01b2   6.6.18                          *

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,19 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
