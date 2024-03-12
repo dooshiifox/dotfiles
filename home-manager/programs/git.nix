3f1eed5c4ea25cf47e93b69da92321e18bfee47b
dooshii <edoosh.yt@gmail.com>
2024-03-13 11:05:11 +1300

    130 current  2024-03-13 11:05:04  24.05.20240301.72f01b2   6.6.18                          *

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -1,6 +1,11 @@
+# Git
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/git.nix
 {
   programs.git = {
     enable = true;
+    aliases = {
+      lgb = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(normal)%s%C(reset) %C(dim white)- %an%C(reset)'";
+    };
     extraConfig = {
       init.defaultBranch = "main";
     };
