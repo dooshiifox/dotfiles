e2ac4c359f13a7935d8d5db409643a6800657c37
dooshii <edoosh.yt@gmail.com>
2024-03-13 13:18:22 +1300

    142 current  2024-03-13 13:18:13  24.05.20240301.72f01b2   6.6.18                          *

--- nix/android.nix
+++ nix/android.nix
@@ -1,8 +1,14 @@
 {pkgs, ...}: let
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/mobile/androidenv/compose-android-packages.nix
   androidComposition = pkgs.androidenv.composeAndroidPackages {
     buildToolsVersions = ["30.0.3" "33.0.0"];
     platformVersions = ["33"];
-    abiVersions = ["armeabi-v7a" "arm64-v8a"];
+    # abiVersions = ["armeabi-v7a" "arm64-v8a"];
+    includeEmulator = true;
+    ndkVersions = ["23.1.7779620"];
+    includeNDK = true;
+    cmakeVersions = ["3.22.1"];
+    includeSources = true;
   };
   androidSdk = androidComposition.androidsdk;
 in {
@@ -14,4 +20,7 @@ in {
     GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/31.0.0/aapt2";
     ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
   };
+  environment.systemPackages = with pkgs; [
+    androidSdk
+  ];
 }
