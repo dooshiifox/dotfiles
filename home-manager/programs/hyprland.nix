c33d7d97278d1366d179061a78aaea545b565d6a
kit <edoosh.yt@gmail.com>
2024-03-29 12:55:01 +1300

    172 current  2024-03-29 12:54:51  24.05.20240301.72f01b2   6.6.18                          *

--- home-manager/programs/hyprland.nix
+++ home-manager/programs/hyprland.nix
@@ -114,8 +114,16 @@
 
       env = [
         "HYPRCURSOR_THEME,Bibata-Modern-Classic"
-        "HYPRCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,16"
       ];
     };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
   };
 }
