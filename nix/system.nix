9b10fc55feda23f6d84846295883bd97fb0768cc
kit <edoosh.yt@gmail.com>
2024-06-19 13:52:57 +1200

    212 current  2024-06-19 13:52:44  24.05.20240301.72f01b2   6.6.18                          *

--- nix/system.nix
+++ nix/system.nix
@@ -65,6 +65,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
 
   ####################
   #   AUDIO
