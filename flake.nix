4cb8747428e176ef8e2cc08a807b8e10bceb1391
kit <edoosh.yt@gmail.com>
2024-11-19 15:03:50 +1300

    272 current  2024-11-19 15:03:35  24.11.20241116.057f63b   6.6.60                          *

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
