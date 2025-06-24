5c43e95231115ce695f713916b2a9d6a2197e33b
kit <edoosh.yt@gmail.com>
2025-06-24 17:36:07 +1200

    361 current  2025-06-24 17:36:04  25.05.20250424.f771eb4  6.14.3                          *

--- home-manager/programs/hyprland.nix
+++ home-manager/programs/hyprland.nix
@@ -15,7 +15,7 @@
         # Home External
         "desc:KOGAN AUSTRALIA PTY LTD KALED24144F,1920x1080@60,0x0,1"
         # Internal
-        "desc:BOE 0x0BB7,3840x2160@144,1920x0,2"
+        "desc:BOE 0x0BB7,3840x2160@120,1920x0,2"
       ];
 
       "$mod" = "SUPER";
