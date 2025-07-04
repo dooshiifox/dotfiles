4f1877f7113ef3d893fe59085d5191352c75038e
kit <edoosh.yt@gmail.com>
2025-07-05 11:52:09 +1200

    449

--- home-manager/programs/nvim/lua/config/options.lua
+++ home-manager/programs/nvim/lua/config/options.lua
@@ -8,5 +8,3 @@ vim.opt.shell = "fish"
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
 vim.cmd("aunmenu PopUp.-2-")
 vim.opt.spelllang = "en_nz,en,en_us,en_uk"
--- Reload the spellcheck file when loading after a nix build
-vim.cmd(":mkspell! %")
