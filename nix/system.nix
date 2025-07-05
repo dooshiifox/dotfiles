a101b7a03206c4d5e3496021d7b38c089a407f58
kit <edoosh.yt@gmail.com>
2025-07-05 16:32:10 +1200

    458

--- nix/system.nix
+++ nix/system.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
@@ -33,9 +34,9 @@
       kernelModules = [ ];
     };
 
-    # extraModprobeConfig = ''
-    #   options nvidia_modeset vblank_sem_control=0
-    # '';
+    extraModprobeConfig = ''
+      options nvidia_modeset vblank_sem_control=0
+    '';
   };
 
   ####################
@@ -173,7 +174,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
@@ -187,13 +188,13 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     # forceFullCompositionPipeline = true;
 
     prime = {
