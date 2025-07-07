5034671253131c5b6f2d2b3475fc1c34f1db89f5
kit <edoosh.yt@gmail.com>
2025-07-07 21:10:24 +1200

    (468) gtk theming

--- home-manager/programs/gtk.nix
+++ home-manager/programs/gtk.nix
@@ -2,37 +2,46 @@
   pkgs,
   THEME,
   ...
-}: {
-  gtk = let
-    extra-config = {
-      gtk-application-prefer-dark-theme =
-        if THEME.scheme == "light"
-        then 0
-        else 1;
-    };
-  in rec {
-    enable = true;
-    gtk4.extraCss = ''
-      .background {
-        background-color: ${THEME.hexWithOpacity THEME.bg THEME.bg-opacity};
-      }
-      .box {
-        background-color: #00000000;
-      }
-    '';
-    gtk3.extraCss = gtk4.extraCss;
+}:
+{
+  gtk =
+    let
+      extra-config = {
+        gtk-application-prefer-dark-theme = if THEME.scheme == "light" then 0 else 1;
+      };
+    in
+    rec {
+      enable = true;
+      gtk4.extraCss = ''
+        * {
+          color: ${THEME.fg};
+        }
 
-    gtk3.extraConfig = extra-config;
-    gtk4.extraConfig = extra-config;
+        .background,
+        contents {
+          background-color: ${THEME.hexWithOpacity THEME.bg THEME.bg-opacity};
+        }
+        .box,
+        popover {
+          background-color: transparent;
+        }
+        highlight {
+          background-color: ${THEME.fg};
+        }
+      '';
+      gtk3.extraCss = gtk4.extraCss;
 
-    iconTheme = {
-      package = pkgs.candy-icons;
-      name = "candy-icons";
-    };
+      gtk3.extraConfig = extra-config;
+      gtk4.extraConfig = extra-config;
 
-    font = {
-      name = THEME.regular-font;
-      size = 11;
+      iconTheme = {
+        package = pkgs.candy-icons;
+        name = "candy-icons";
+      };
+
+      font = {
+        name = THEME.regular-font;
+        size = 11;
+      };
     };
-  };
 }
