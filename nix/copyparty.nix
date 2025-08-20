3ce1a018d9c734dcc2fb4c0413e2acd7c969beb3
kit <edoosh.yt@gmail.com>
2025-08-21 11:32:41 +1200

    (507) Copyparty perms

--- nix/copyparty.nix
+++ nix/copyparty.nix
@@ -4,7 +4,9 @@
     enable = true;
 
     settings = {
+      # IP
       i = "0.0.0.0";
+      # Port
       p = 3210;
     };
 
@@ -15,12 +17,23 @@
     volumes = {
       "/" = {
         path = "/data";
+        # Permissions
+        # https://github.com/9001/copyparty/?tab=readme-ov-file#accounts-and-volumes
         access = {
           r = "*";
           A = [ "dooshii" ];
         };
         flags = {
+          # Rescan files every 60 seconds
+          # https://github.com/9001/copyparty/?tab=readme-ov-file#file-indexing
           scan = 60;
+          # Indexing of files and song tags. Allows searching
+          # https://github.com/9001/copyparty/?tab=readme-ov-file#file-indexing
+          e2dsa = true;
+          e2ts = true;
+          # Announces fileserver on file explorers
+          # https://github.com/9001/copyparty/?tab=readme-ov-file#zeroconf
+          z = true;
         };
       };
     };
